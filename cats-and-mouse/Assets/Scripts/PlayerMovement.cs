b6a62c7c5baefd6b85fbe6f059b6562f74e9416f
Ky Kim <kyungjinkim94@gmail.com>
2020-03-14 16:38:36 -0400

    added player movement and steering arrive AI movement
    added player movement and steering arrive AI movement

---
+++ cats-and-mouse/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Transform playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (speed <= 0)
+            speed = 0.5f;
+        if (playerCamera == null)
+            playerCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(x) > 0 || Mathf.Abs(y) > 0)
+        {
+            float angle = Mathf.Atan2(-x, y) * 180 / Mathf.PI;
+            Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10 * Time.fixedDeltaTime);
+        }
+
+        transform.position += (Vector3)(speed * new Vector2(x, y) * Time.fixedDeltaTime);
+        playerCamera.position = transform.position + (Vector3.forward * -10);
+    }
+}
---
+++ cats-and-mouse/Assets/Scripts/SteeringArrive.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringArrive : MonoBehaviour
+{
+    [SerializeField] Vector2 target;
+    [SerializeField] float radiusSlow;
+    [SerializeField] float radiusArrive;
+    [SerializeField] float maxVelocity;
+    [SerializeField] float maxAcceleration;
+    [SerializeField] float t2t;
+
+    private Vector2 velocity;
+    private Vector2 acceleration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Vector2.zero;
+        Debug.Log(velocity.magnitude);
+        acceleration = Vector2.zero;
+        target = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetRandomTarget();
+        Reorient();
+        Arrive();
+    }
+
+    // Set target to a random location
+    // adjust the values in Random.Range() to adjust possible values
+    private void SetRandomTarget()
+    {
+        float x = Random.Range(-10f, 10f);
+        float y = Random.Range(-5, 5);
+        target = new Vector2(x, y);
+    }
+
+    // Update gameobject's position based on velocity
+    // p = p + delta_v
+    private void UpdatePosition(float time)
+    {
+        Vector3 deltaPos = time * velocity;
+        transform.position += deltaPos;
+    }
+
+    // Update gameobject's velocity based on acceleration
+    // v = v + delta_a
+    private void UpdateVelocity(float time)
+    {
+        this.velocity += time * acceleration;
+        if (this.velocity.magnitude > this.maxVelocity)
+            this.velocity = this.velocity.normalized * this.maxVelocity;
+    }
+
+    // Steering Arrive
+    private void Arrive()
+    {
+        Vector2 displacementVector = target - (Vector2)transform.position;
+        float magnitude = displacementVector.magnitude;
+        Vector2 directionalVector = displacementVector.normalized;
+
+        if (magnitude > radiusSlow)
+        {
+            this.acceleration = maxAcceleration * directionalVector;
+        }
+        else if (magnitude > radiusArrive)
+        {
+            float goalSpeed = this.maxVelocity * (magnitude / radiusSlow);
+            Vector2 goalVelocity = directionalVector * goalSpeed;
+            this.acceleration = (goalVelocity - this.velocity) / this.t2t;
+        }
+        else
+        {
+            this.acceleration = Vector2.zero;
+            this.velocity = Vector2.zero;
+            return;
+        }
+
+        if (this.acceleration.magnitude > this.maxAcceleration)
+            this.acceleration = this.acceleration.normalized * this.maxAcceleration;
+        UpdateVelocity(Time.fixedDeltaTime);
+        UpdatePosition(Time.fixedDeltaTime);
+    }
+
+    // Reorient to face the moving direction
+    private void Reorient()
+    {
+        if (velocity.magnitude > 0)
+        {
+            float angle = Mathf.Atan2(-velocity.x, velocity.y) * 180 / Mathf.PI;
+            Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10 * Time.fixedDeltaTime);
+        }
+    }
+
+    // Set Target to a particular position.
+    public void SetTarget(Vector2 position)
+    {
+        this.target = position;
+    }
+}
