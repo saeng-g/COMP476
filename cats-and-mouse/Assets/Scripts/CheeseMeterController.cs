aafbf2918768c3d98f7639cf93f0b0875b5206eb
Ky Kim <kyungjinkim94@gmail.com>
2020-04-14 09:08:33 -0400

    Player Animation + Cheese meter GUI

---
+++ cats-and-mouse/Assets/Scripts/CheeseMeterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheeseMeterController : MonoBehaviour
+{
+    [SerializeField] Image image;
+    [SerializeField] PlayerMovement player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (image == null)
+            image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = player.score / PlayerMovement.MAXCHEESE;
+    }
+}
